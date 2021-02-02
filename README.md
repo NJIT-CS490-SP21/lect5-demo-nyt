ff591c54e4a0558a614848058cc1d8010e2985ec
Naman Agrawal <na77@njit.edu>
2021-02-02 04:25:08 +0000

    Add setup info to README, comments to .py

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Lecture 5 Demo - NYT API
 
-Create .env file in your main directory
-Add line: `export NYT_KEY='YOUR_KEY'`
+## Requirements
+1. `pip install python-dotenv`
+2. `pip install requests`
+
+## Setup
+1. Create `.env` file in your main directory
+2. Add your NYT key from https://developer.nytimes.com/my-apps with the line: `export NYT_KEY='YOUR_KEY'`
+
+## Run Application
+1. Run command in terminal `python lect5-demo.py`
+2. See output for Gamestop articles 
--- lect5-demo.py
+++ lect5-demo.py
@@ -2,17 +2,18 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 
-BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
-
 load_dotenv(find_dotenv()) # This is to load your API keys from .env
 
+BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+
 params = {
-    'q': 'gamestop',
+    'q': 'gamestop', # Query keywords
     'api-key': os.getenv('NYT_KEY'),
 }
 
 response = requests.get(BASE_URL, params=params)
 data = response.json()
 
+# Print headlines for first 10 articles returned by search endpoint
 for i in range(0, 10):
     print(data['response']['docs'][i]['headline']['main'])
