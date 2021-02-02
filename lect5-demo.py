41329e662f1d1c7a13dc82f7d51f618d02f5f0dc
Naman Agrawal <na77@njit.edu>
2021-02-02 04:12:24 +0000

    Initial commit, add gitignore and demo python file

---
+++ lect5-demo.py
@@ -0,0 +1,18 @@
+import requests
+import os
+from dotenv import load_dotenv, find_dotenv
+
+BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+
+load_dotenv(find_dotenv())
+
+params = {
+    'q': 'gamestop',
+    'api-key': os.getenv('NYT_KEY'),
+}
+
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+
+for i in range(0, 10):
+    print(data['response']['docs'][i]['headline']['main'])
